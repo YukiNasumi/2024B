16ae536d027b29f3a96c2b9a74c8d2d26af1afad
YukiNasumi <ireneenriquez657@gmail.com>
2024-09-07 18:41:43 +0800

    修改了q2

---
+++ case_dict.py
@@ -0,0 +1,14 @@
+case = {
+    'p1':{ 'fail':0.1,'cost':2,'test':1},
+    'p2':{ 'fail':0.1,'cost':8,'test':1},
+    'p3':{ 'fail':0.1,'cost':12,'test':2},
+    'p4':{ 'fail':0.1,'cost':2,'test':1},
+    'p5':{ 'fail':0.1,'cost':8,'test':1},
+    'p6':{ 'fail':0.1,'cost':12,'test':2},
+    'p7':{ 'fail':0.1,'cost':8,'test':1},
+    'p8':{ 'fail':0.1,'cost':12,'test':2},
+    'h1':{'fail':0.1,'cost':8,'test':4,'disassemble':6},
+    'h2':{'fail':0.1,'cost':8,'test':4,'disassemble':6},
+    'h3':{'fail':0.1,'cost':8,'test':4,'disassemble':6},
+    'f':{'fail':0.1,'cost':8,'test':6,'disassemble':10,'market':200,'adjustment':40}#³ÉÆ·
+}
